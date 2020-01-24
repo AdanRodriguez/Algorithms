c80c6a9ac456991348f2bd142336dc72bddc2470
Adan <adanrodriguezevo@gmail.com>
2020-01-23 18:25:31 -0600

    MVP

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -2,8 +2,28 @@
 
 import sys
 
-def making_change(amount, denominations):
-  pass 
+def making_change(amount, denominations, cache = 0):
+  ways = [0 for i in range(amount + 1)]
+
+  #base case
+  ways[0] = 1
+
+  #for loop -> for value in denominations, return amount of ways
+  for value in denominations:
+    # for a given coin, loop through all of the higher amounts between our coin and
+    # the amount (i.e., `for higher_amount in range(coin, amount + 1)`)
+    # for loop2 -> for the higher_amount in range(1, amount + 1)
+    for higher_amount in range(1, amount + 1):
+       # if the denom is less than or equal to the current amount
+        if value <= higher_amount:
+            # Take the difference between the higher amount and the value of coin
+            # start building up the values in cache.
+            # then increment at the amount's position in ways
+            ways[higher_amount] += ways[higher_amount - value]
+
+  return ways[amount]
+
+
 
 
 if __name__ == "__main__":
--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,8 +2,31 @@
 
 import sys
 
+# define what the options are -> separated like in the test file line 8
+options = [["rock"], ["paper"], ["scissors"]]
+
 def rock_paper_scissors(n):
-  pass 
+      # if n is equal to 0, return empty list -> from line 7 of the test file
+    if n == 0:
+        return [[]]
+
+    # if n is equal to 1, return the options
+    if n == 1:
+        return options
+
+    # set the output to an empty list
+    output = []
+
+    # set a list equal to the rock_paper_scissors n - 1
+    listA = rock_paper_scissors(n - 1)
+    # for loop -> the sub list in the list, return the output
+    for subList in listA:
+        # for loop -> action in the options, set new action equal to the sub
+        # list + the action, then append the output with the new action
+        for play in options:
+            newPlay = subList + play
+            output.append(newPlay)
+    return output
 
 
 if __name__ == "__main__":
